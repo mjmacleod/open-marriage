ab842b8b093ef9a404b9d70ec2eac1378a595674
Malcolm MacLeod <mmacleod@webmail.co.za>
2015-10-11 19:21:23 +0200

    .

--- public/js/maps.js
+++ public/js/maps.js
@@ -4,16 +4,11 @@ YUI.add('MAC-maps', function (Y) {
 
     Y.all('[data-map]').each(function (mapNode) {
          L.mapbox.accessToken = 'pk.eyJ1IjoibW1hY2xlb2QiLCJhIjoiY2lmbWlkamV0MDE2cHU2bHlyc2p3Y3pwbiJ9.YjHJqXvs_YB9bSQpJQWtrw';
-         var map = L.mapbox.map('map', 'mmacleod.nm6dohm7');
-
-var featureLayer = L.mapbox.featureLayer().addTo(map);
+         var map = L.mapbox.map('map', 'mmacleod.nm6dohm7', {zoomControl: false});
+         var featureLayer = L.mapbox.featureLayer().addTo(map);
          featureLayer.loadID("mmacleod.nm6dohm7");
-
-featureLayer.on('ready', function() {
-    // featureLayer.getBounds() returns the corners of the furthest-out markers,
-    // and map.fitBounds() makes sure that the map contains these.
-    map.fitBounds(featureLayer.getBounds(), { padding: [250, 250] });
-});
+         new L.Control.Zoom({position:'bottomright'}).addTo(map);
+         featureLayer.on('ready', function() {map.fitBounds(featureLayer.getBounds());});
     })
 
 }, '1.8.0', {

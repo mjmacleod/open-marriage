a77d503da45eecd948f523b9aa2bec6fe8af24cf
Malcolm MacLeod <mmacleod@webmail.co.za>
2015-10-11 16:00:21 +0200

    .

--- public/js/maps.js
+++ public/js/maps.js
@@ -4,7 +4,8 @@ YUI.add('MAC-maps', function (Y) {
 
     Y.all('[data-map]').each(function (mapNode) {
          L.mapbox.accessToken = 'pk.eyJ1IjoibW1hY2xlb2QiLCJhIjoiY2lmbWlkamV0MDE2cHU2bHlyc2p3Y3pwbiJ9.YjHJqXvs_YB9bSQpJQWtrw';
-         var map = L.mapbox.map('map', 'mmacleod.nm6dohm7');
+//         var map = L.mapbox.map('map', 'mmacleod.nm6dohm7');
+var map = L.mapbox.map(mapNode.getDOMNode(), [data.layer,data.markers], null, [MM.DoubleClickHandler(),MM.DragHandler()]);
          map.ui.zoomer.add();
          map.centerzoom(data.center, data.zoom);
     })

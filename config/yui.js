99aac0dd18f98dcbef352599102d210dc16571cd
Malcolm MacLeod <mmacleod@webmail.co.za>
2015-10-11 15:48:15 +0200

    .

--- config/yui.js
+++ config/yui.js
@@ -10,10 +10,10 @@ exports.config  = {
     root   : YUI_VERSION + '/',
 
     modules: {
-        'mapbox-css': 'http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.css',
+        'mapbox-css': 'https://api.mapbox.com/mapbox.js/v2.2.2/mapbox.js',
 
         'mapbox': {
-            fullpath: 'http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.js',
+            fullpath: 'https://api.mapbox.com/mapbox.js/v2.2.2/mapbox.js',
             requires: ['mapbox-css']
         }
     },

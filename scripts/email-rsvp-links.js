33f7d8088c21b92b2ca9d9f98fcf76e639f0503f
Malcolm MacLeod <mmacleod@webmail.co.za>
2015-10-12 23:03:06 +0200

    .

--- scripts/email-rsvp-links.js
+++ scripts/email-rsvp-links.js
@@ -41,14 +41,11 @@ function sendEmail(invitation, callback) {
     }
     else
     {
-    email.sendRsvpLink(invitation, function (err, res, body) {
-            if (err)
-            {
-                console.log(err);
-            } else {
-                console.log('Sent RSVP link to Invitation: ' + invitation.id + ' recipients.');
-            }
+        email.sendRsvpLink(invitation, "", function (err) {
+        if (err)
+            {console.log(err); }
+        else
+            {console.log('Sent RSVP link to Invitation: ' + invitation.id + ' recipients.'); };
         callback(null);
-    });
-   } 
+    }
 }
